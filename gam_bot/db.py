48d84c22dd062c2720dd89845c3e83836b2134fe
Cameron Graybill <camerongraybill@gmail.com>
2021-10-24 15:04:17 -0400

    Cleaning up running app a little better.

--- gam_bot/db.py
+++ gam_bot/db.py
@@ -6,3 +6,7 @@ from gam_bot.settings import DB_CONN_STR
 async def init_db() -> None:
     await Tortoise.init(db_url=DB_CONN_STR, modules={"models": ["gam_bot.models"]})
     await Tortoise.generate_schemas()
+
+
+async def close_db() -> None:
+    await Tortoise.close_connections()

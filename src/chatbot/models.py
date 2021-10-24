aea5ca52dbd6d3d522ee189debda313be8888c21
Daron Arakelian <daronarakelian@gmail.com>
2021-10-24 18:27:52 -0400

    Add social_score

--- src/chatbot/models.py
+++ src/chatbot/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class GamUser(models.Model):
     discord_id = models.IntegerField(primary_key=True)
     gam_coins = models.PositiveIntegerField(default=0)
+    social_score = models.PositiveIntegerField(default=0)

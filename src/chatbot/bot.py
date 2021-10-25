d7f7e087d9a76773d650f4303aac13603e156926
Daron Arakelian <daronarakelian@gmail.com>
2021-10-24 20:26:11 -0400

    Fix linting

--- src/chatbot/bot.py
+++ src/chatbot/bot.py
@@ -1,6 +1,4 @@
 from logging import getLogger
-from os import stat
-from typing import Text
 
 import discord
 from discord import Message, GroupChannel, TextChannel, RawReactionActionEvent
@@ -32,8 +30,8 @@ class Bot(discord.Client):
 
     @staticmethod
     @sync_to_async
-    def get_gam_user(id: int) -> GamUser:
-        user, _ = GamUser.objects.get_or_create(discord_id=id)
+    def get_gam_user(user_id: int) -> GamUser:
+        user, _ = GamUser.objects.get_or_create(discord_id=user_id)
         return user
 
     @staticmethod
@@ -47,12 +45,12 @@ class Bot(discord.Client):
             message = await channel.fetch_message(payload.message_id)
             user: GamUser = await Bot.get_gam_user(message.author.id)
             emoji_str = str(payload.emoji)
-            logger.debug(f"User's current social score is {user.social_score}")
+            logger.debug("User's current social score is %d", user.social_score)
             if emoji_str == settings.ADD_SOCIAL_SCORE:
                 user.social_score += 1
             elif emoji_str == settings.REMOVE_SOCIAL_SCORE:
                 user.social_score -= 1
-            logger.debug(f"User's new social score is {user.social_score}")
+            logger.debug("User's new social score is %d", user.social_score)
             await Bot.save_gam_user(user)
 
     async def on_raw_reaction_remove(self, payload: RawReactionActionEvent) -> None:
@@ -61,10 +59,10 @@ class Bot(discord.Client):
             message = await channel.fetch_message(payload.message_id)
             user: GamUser = await Bot.get_gam_user(message.author.id)
             emoji_str = str(payload.emoji)
-            logger.debug(f"User's current social score is {user.social_score}")
+            logger.debug("User's current social score is %d", user.social_score)
             if emoji_str == settings.ADD_SOCIAL_SCORE:
                 user.social_score -= 1
             elif emoji_str == settings.REMOVE_SOCIAL_SCORE:
                 user.social_score += 1
-            logger.debug(f"User's new social score is {user.social_score}")
+            logger.debug("User's new social score is %d", user.social_score)
             await Bot.save_gam_user(user)

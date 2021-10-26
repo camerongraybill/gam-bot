779c54934e4db98052f399b58dbee5261d719081
John Zlotek <jzlotek@gmail.com>
2021-10-26 17:53:20 -0400

    linting bad

--- src/chatbot/test_command.py
+++ src/chatbot/test_command.py
@@ -1,33 +1,38 @@
 from unittest import IsolatedAsyncioTestCase
-from mock import Mock
+from unittest.mock import Mock
+from typing import Sequence, Optional
 from .command import Command  # pylint: disable=import-error
+from discord import Message
 
 
 class CommandTest(IsolatedAsyncioTestCase):
-    def setUp(self):
+    def setUp(self) -> None:
         self.message = Mock()
 
-    async def test_command_empty_channels(self):
+    async def test_command_empty_channels(self) -> None:
         command = Command("test", None, ("Response",))
         self.assertTrue(command.match("channel"))
         self.assertSequenceEqual(
             await command(self.message, "channel", "test"), ("Response",)
         )
 
-    async def test_command_empty_response(self):
+    async def test_command_empty_response(self) -> None:
         command = Command(keyword="test", channels={"channel"})
         self.assertTrue(command.match("channel"))
         response = await command(self.message, "channel", "test")
         self.assertEqual(len(response), 0)
 
-    async def test_command_match_regex(self):
+    async def test_command_match_regex(self) -> None:
         command = Command("test", None, ("Response",))
         self.assertSequenceEqual(
             await command(self.message, "channel", "test muh command"), ("Response",)
         )
 
-    async def test_command_lambda_returns_arguments(self):
-        async def test_func(message, channel, args):  # pylint: disable=unused-argument
+    async def test_command_lambda_returns_arguments(self) -> None:
+        # pylint: disable=unused-argument
+        async def test_func(
+            message: Message, channel: Optional[Sequence[str]], args: Sequence[str]
+        ) -> Sequence[str]:
             return args
 
         command = Command("test", None, ("Response",), func=test_func)

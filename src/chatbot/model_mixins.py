ca8a6073ece0bb6c9a87f85cd4177a7ba644348f
Daron Arakelian <daronarakelian@gmail.com>
2021-11-02 23:24:37 -0400

    Fix bandit

--- src/chatbot/model_mixins.py
+++ src/chatbot/model_mixins.py
@@ -18,18 +18,12 @@ class AsyncModelMixin:
 
     @classmethod
     def async_qs(cls: Type[_T]) -> AsyncEnabledQuerySet[_T]:  # type: ignore
-        assert isinstance(cls.objects, AsyncEnabledManager)
+        if not isinstance(cls.objects, AsyncEnabledManager):
+            raise AssertionError()
         return cls.objects.all()  # type: ignore
 
-    async def async_destroy(self: _T) -> None:  # type: ignore
-        @better_sync_to_async
-        def _() -> None:
-            self.delete()
-
-        await _()
-
     async def async_delete(self) -> None:
-        @sync_to_async
+        @better_sync_to_async
         def _() -> None:
             self.delete()  # type: ignore
 

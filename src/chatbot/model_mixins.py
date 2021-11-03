b077c93c91f4a4598fea204f4cb1baee857a5747
Cameron Graybill <camerongraybill@gmail.com>
2021-11-02 23:10:49 -0400

    More async helpers - adds a better way to get an async queryset

--- src/chatbot/model_mixins.py
+++ src/chatbot/model_mixins.py
@@ -1,11 +1,29 @@
-from asgiref.sync import sync_to_async
+from typing import Type, TypeVar
+
+from django.db.models import Model
+
+from chatbot.managers import AsyncEnabledManager
+from chatbot.querysets import AsyncEnabledQuerySet, better_sync_to_async
+
+_T = TypeVar('_T', bound=Model)
 
 
-# pylint: disable=too-few-public-methods
 class AsyncModelMixin:
-    async def async_save(self) -> None:
-        @sync_to_async
+    async def async_save(self: _T) -> None:  # type: ignore
+        @better_sync_to_async
         def _() -> None:
-            self.save()  # type: ignore
+            self.save()
+
+        await _()
+
+    @classmethod
+    def async_qs(cls: Type[_T]) -> AsyncEnabledQuerySet[_T]:  # type: ignore
+        assert isinstance(cls.objects, AsyncEnabledManager)
+        return cls.objects.all()  # type: ignore
+
+    async def async_destroy(self: _T) -> None:  # type: ignore
+        @better_sync_to_async
+        def _() -> None:
+            self.delete()
 
         await _()

2a1df0c7b2216211c98b4c59551c1a91f46496af
Daron Arakelian <daronarakelian@gmail.com>
2021-11-03 21:32:46 -0400

    Black worst tool NA

--- src/chatbot/migrations/0007_merge_0005_emojiscore_0006_rename_bettor_wager_user.py
+++ src/chatbot/migrations/0007_merge_0005_emojiscore_0006_rename_bettor_wager_user.py
@@ -6,9 +6,8 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('chatbot', '0005_emojiscore'),
-        ('chatbot', '0006_rename_bettor_wager_user'),
+        ("chatbot", "0005_emojiscore"),
+        ("chatbot", "0006_rename_bettor_wager_user"),
     ]
 
-    operations = [
-    ]
+    operations = []

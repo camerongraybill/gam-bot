855b321105d6392d41b9c9c93afa2b73df810500
Cameron Graybill <camerongraybill@gmail.com>
2021-11-04 22:33:10 -0400

    Adding migrations for discord_bot

--- src/discord_bot/managers.py
+++ src/discord_bot/managers.py
@@ -10,4 +10,4 @@ _T = TypeVar('_T', bound='DiscordUser')
 
 class DiscordUserManager(AsyncEnabledManager[_T]):
     async def lookup_user(self, discord_id: int) -> _T:
-        return await self.async_get_or_create(discord_id=discord_id)
+        return await self.async_get_or_create(discord_id=discord_id)[0]

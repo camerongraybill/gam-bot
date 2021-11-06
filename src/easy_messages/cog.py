fd80d639c603a91094c8fbc23099e65d80ad8835
Cameron Graybill <camerongraybill@gmail.com>
2021-11-06 00:49:31 -0400

    Linter fix

--- src/easy_messages/cog.py
+++ src/easy_messages/cog.py
@@ -3,21 +3,21 @@ from __future__ import annotations
 from typing import Optional, Sequence, TYPE_CHECKING, Any
 
 from discord.ext import commands
-from discord.ext.commands import Context, Bot
+from discord.ext.commands import Context
 
 from discord_bot.cog import BaseCog
 from discord_bot.checks import is_in_channel
 from . import settings
 
 if TYPE_CHECKING:
-    from discord.ext.commands import Command
+    from discord.ext.commands import Command, Bot
 
 
 class EasyCog(BaseCog):
-    def __new__(cls, bot: Bot['Context']) -> Any:
+    def __new__(cls, bot: "Bot[Context]") -> Any:
         for cmd in settings.COMMANDS:
             cls.__cog_commands__.append(build_command(*cmd))
-        return super().__new__(cls, bot)
+        return super().__new__(cls, bot)  # type: ignore
 
 
 def build_command(

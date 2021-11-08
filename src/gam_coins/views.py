1f7984614c7129de8f9dc5689043630e3af1aad5
Cameron Graybill <camerongraybill@gmail.com>
2021-11-08 12:25:49 -0500

    Fixing leaderboards

--- src/gam_coins/views.py
+++ src/gam_coins/views.py
@@ -5,7 +5,7 @@ from .models import Account, Wager
 
 
 class LeaderboardView(TemplateView):
-    template_name = "leaderboard.html"
+    template_name = "coin_leaderboard.html"
 
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
--- src/social_score/views.py
+++ src/social_score/views.py
@@ -6,7 +6,7 @@ from .models import SocialScore
 
 
 class LeaderboardView(TemplateView):
-    template_name = "leaderboard.html"
+    template_name = "social_leaderboard.html"
 
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
